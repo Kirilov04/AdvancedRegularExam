3a48348ee9596202522e826f6b08300f762c21e9
Kirilov04 <teodorkirilov2004@icloud.com>
2024-02-17 13:17:26 +0200

    Add project files.

---
+++ AdvancedRegularExam/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Stack<int> amountOfMoney = new Stack<int>(Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray());
+
+        Queue<int> priceSize = new Queue<int>(Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray());
+
+        int countOfStack = amountOfMoney.Count;
+        int henryAteFoods = 0;
+        int henryResto = 0;
+
+        while (amountOfMoney.Count > 0 && priceSize.Count > 0)
+        {
+            int AmountMoney = amountOfMoney.Pop();
+            int priceSizes = priceSize.Dequeue();
+
+            if (AmountMoney == priceSizes)
+            {
+                henryAteFoods++;
+            }
+            else if (AmountMoney > priceSizes)
+            {
+                henryAteFoods++;
+                henryResto += AmountMoney - priceSizes;
+            }
+            else if (AmountMoney < priceSizes)
+            {
+                AmountMoney += henryResto;
+                if (AmountMoney >= priceSizes)
+                {
+                    henryAteFoods++;
+                    henryResto -= AmountMoney - priceSizes;
+
+                    if (AmountMoney - priceSizes == 0)
+                    {
+                        henryResto = 0;
+                    }
+                }
+            }
+        }
+
+        if ( henryAteFoods == countOfStack)
+        {
+            Console.WriteLine($"Gluttony of the day! Henry ate {henryAteFoods} foods.");
+        }
+        else if (henryAteFoods > 0)
+        {
+            if (henryAteFoods == 1)
+            {
+                Console.WriteLine($"Henry ate: {henryAteFoods} food.");
+            }
+            else
+            {
+                Console.WriteLine($"Henry ate: {henryAteFoods} foods.");
+            }
+            
+        }
+        else if (henryAteFoods == 0) 
+        {
+            Console.WriteLine("Henry remained hungry. He will try next weekend again.");
+        }
+
+
+    }
+}
---
+++ ClearSkies/Program.cs
@@ -0,0 +1,70 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+char[,] airSpace = new char[n, n];
+
+int row = 0, col = 0, enemies = 0,
+    armour = 300;
+
+for (int i = 0; i < n; i++)
+{
+    string line = Console.ReadLine();
+    for (int j = 0; j < n; j++)
+    {
+        airSpace[i, j] = line[j];
+        if (line[j] == 'J')
+        {
+            row = i;
+            col = j;
+            airSpace[i, j] = '-';
+        }
+        else if (line[j] == 'E') enemies++;
+    }
+}
+while (true)
+{
+    string command = Console.ReadLine();
+    switch (command)
+    {
+        case "up": row--; break;
+        case "down": row++; break;
+        case "left": col--; break;
+        case "right": col++; break;
+    }
+
+    if (airSpace[row, col] == 'E')
+    {
+        if (enemies == 1)
+        {
+            Console.WriteLine("Mission accomplished, you neutralized the aerial threat!");
+            break;
+        }
+        else
+        {
+            airSpace[row, col] = '-';
+            armour -= 100;
+            enemies--;
+            if (armour == 0)
+            {
+                Console.WriteLine($"Mission failed, your jetfighter was shot down! Last coordinates [{row}, {col}]!");
+                break;
+            }
+        }
+    }
+
+    else if (airSpace[row, col] == 'R')
+    {
+        armour = 300;
+        airSpace[row, col] = '-';
+    }
+}
+
+airSpace[row, col] = 'J';
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(airSpace[i, j]);
+    }
+    Console.WriteLine();
+}
